4831d017405ee13031f814bec2227b8278169264
Akash Chaudhary <akash.chaudhary@live.in>
2022-07-07 17:43:54 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | School Managment </title>
+
+    <!--Cerulean - Bootswatch theme(Bootstrap v5.1.3) -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/cerulean/bootstrap.min.css' />
+
+
+</head>
+
+<body>
+
+
+</body>
+
+</html>
